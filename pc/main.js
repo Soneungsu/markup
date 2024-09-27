4bf90e6e62748a6c89fe55bc5a63e0ca3b515368
eungsu <eungsu1217@gmail.com>
2024-09-27 23:28:03 +0900

    ing

--- pc/main.js
+++ pc/main.js
@@ -1,1 +1,18 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tab");
+  const tabPanes = document.querySelectorAll(".section_pane");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      tabs.forEach((t) => t.classList.remove("active"));
+      tabPanes.forEach((pane) => pane.classList.remove("active"));
+
+      tab.classList.add("active");
+      const index = [...tabs].indexOf(tab);
+      tabPanes[index].classList.add("active");
+    });
+  });
+});
