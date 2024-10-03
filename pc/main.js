88dcbaa498a802354737cf976126064db6c18eae
eungsu <eungsu1217@gmail.com>
2024-10-03 23:53:51 +0900

    done

--- pc/main.js
+++ pc/main.js
@@ -3,16 +3,19 @@
 // nav버튼 클릭시 보여지고 가려지기
 const navBtn = document.querySelector(".nav_btn");
 const navMenu = document.querySelector(".nav_items");
+const overLayout = document.querySelector(".overlayout");
 let isMenu = true;
 navBtn.addEventListener("click", (e) => {
   //   console.log("gg");
   e.preventDefault();
   isMenu = !isMenu;
   navMenu.style.display = isMenu ? "block" : "none";
+  if (navMenu.style.display === "block") {
+    overLayout.style.display = "block";
+  }
 });
 
-
-//해당 탭 버튼 클릭시 active 추가, 
+//해당 탭 버튼 클릭시 active 추가,
 
 document.addEventListener("click", () => {
   const tabs = document.querySelectorAll(".tab");
